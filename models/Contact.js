87ab9b2528c186dddae91ae4ccb721f8cc93afb2
Chandrika Sista <cguntur@gmail.com>
2024-05-09 14:41:29 -0700

    Added the database tables and som sample seed data to test

---
+++ models/Contact.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Contact extends Model {}
+
+Contact.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    address_1: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    address_2: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    city: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    state: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    member_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model:'member',
+          key: 'id'
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'contact'
+  }
+);
+
+module.exports = Contact;
