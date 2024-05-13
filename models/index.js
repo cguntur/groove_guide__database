31755716b35dc018b4b5c608423bcc52b2413f12
Chadi hoayeK <chadihoayek@Chadis-MacBook-Pro.local>
2024-05-13 11:24:27 -0700

    added foreign key associations, handling the models Roles

--- models/index.js
+++ models/index.js
@@ -6,3 +6,17 @@ const Contact = require('./Contact');
 module.exports = { Role, Member, Contact };
 
 //add foreign key associations
+// Path: models/Role.js
+
+const { Role } = require('./Role'); // Import 'Role' from the correct file
+
+Role.associate = function(models) {
+    Role.hasMany(models.Member, {
+        foreignKey: 'roleId'
+    });
+}
+
+module.exports = Role;
+
+
+
