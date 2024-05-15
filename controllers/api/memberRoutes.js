aa1cf47765041f137a2a62eb8d7ebfe39f280dda
themuppetman <157922635+themuppetman@users.noreply.github.com>
2024-05-14 20:52:22 -0700

    login.js and member routing

--- controllers/api/memberRoutes.js
+++ controllers/api/memberRoutes.js
@@ -3,33 +3,65 @@ const { Member }=require("../../models")
 // const withAuth = require('../../utils/auth');
  
 //GET all members
-router.get('/', (req, res) => {
-    Member.findAll()
-   .then(dbMemberData => res.json(dbMemberData))
-   .catch(err => {
-       console.log(err);
-       res.status(500).json(err);
-   });
-});
-
-//GET one member by id
-router.get('/:id', (req, res) => {
-    Member.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-   .then(dbMemberData => {
-        if (!dbMemberData) {
-            res.status(404).json({ message: 'No member found with this id' });
-            return;
-        }
-        res.json(dbMemberData);
-    })
-   .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+// router.get('/', (req, res) => {
+    // Member.findAll()
+//    .then(dbMemberData => res.json(dbMemberData))
+//    .catch(err => {
+    //    console.log(err);
+    //    res.status(500).json(err);
+//    });
+// });
+// 
+// GET one member by id
+// router.get('/:id', (req, res) => {
+    // Member.findOne({
+        // where: {
+            // id: req.params.id
+        // }
+    // })
+//    .then(dbMemberData => {
+        // if (!dbMemberData) {
+            // res.status(404).json({ message: 'No member found with this id' });
+            // return;
+        // }
+        // res.json(dbMemberData);
+    // })
+//    .catch(err => {
+        // console.log(err);
+        // res.status(500).json(err);
+    // });
+// });
+// 
+router.post('/login', async (req, res) => {
+    try {
+      const memberData = await Member.findOne({ where: { email: req.body.email } });
+  
+      if (!memberData) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      const validPassword = await memberData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res
+          .status(400)
+          .json({ message: 'Incorrect email or password, please try again' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.member_id = memberData.id;
+        req.session.logged_in = true;
+        
+        res.json({ member: memberData, message: 'You are now logged in!' });
+      });
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
 });
 
 //Create new member
@@ -40,7 +72,7 @@ router.post('/signup', async (req, res) => {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
         // Create new user
-        const newUser = await Member.create({
+        const newMember = await Member.create({
             email: req.body.email,
             password: hashedPassword,
         });
@@ -48,7 +80,7 @@ router.post('/signup', async (req, res) => {
         // We might want to log the user in directly after signup
         // For example, by setting some session variables
 
-        res.status(201).json(newUser);
+        res.status(201).json(newMember);
     } catch (err) {
         console.error(err);
         res.status(500).json(err);
