ad9923cda6974688408e103bd30db49604553069
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-05-15 19:02:35 -0700

    ran git for member info for member page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { Member, Contact, Role } = require('../models');
 
 router.get('/', async (req, res) => {
   // sending rendered Handlebars.js template to respod
@@ -12,7 +13,17 @@ router.get('/login', async (req, res) => {
 
 router.get('/member', async (req, res) => {
   // sending rendered Handlebars.js template to respod
-  res.render('member_page');
+  try {
+      const memberData = await Member.findByPk(req.session.member_id)
+      const member = memberData.get({plain:true})
+      res.render('member_page', {
+        logged_in:req.session.logged_in,
+        member
+      });
+  } catch (error) {
+    res.status(500).json(error)
+  }
+  
 });
 
 
