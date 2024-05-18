a5431b73edc0c0c607d4678e0c41fbb5cd9843f8
Chandrika Sista <cguntur@gmail.com>
2024-05-18 11:34:53 -0700

    Updated the styles for the forms, added the api to get the contact information for the members and display them on the member page

--- public/js/member.js
+++ public/js/member.js
@@ -1,3 +1,10 @@
+document.querySelector('.add-address').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log("Add address button clicked");
+    document.querySelector('.contact-form').classList.remove('hide');
+});
+
+
 //Create an event listener and added to the contact table
 
 //when the user fills out the contact form, we need to add the contact information to the contact table.
@@ -9,10 +16,12 @@
 const contactFormHandler = async (event) => {
     event.preventDefault();
 
+    const address_type = document.querySelector('#member_address_type').value.trim();
     const address_1 = document.querySelector('#member_address_1').value.trim();
     const address_2 = document.querySelector('#member_address_2').value.trim();
     const city = document.querySelector('#member_city').value.trim();
     const state = document.querySelector("#member_state").value.trim();
+    const zip = document.querySelector("#member_zip").value.trim();
     const country = document.querySelector("#member_country").value.trim();
     
 
@@ -20,16 +29,15 @@ const contactFormHandler = async (event) => {
         //Send a POST request to the API endpoint
         const response = await fetch('/api/contacts', {
             method: 'POST',
-            body: JSON.stringify({ address_1, address_2, city, state, country}),
+            body: JSON.stringify({ address_type, address_1, address_2, city, state, zip, country}),
             headers: { 'Content-Type': 'application/json' },
         });
         if(response.ok){
-            document.location.replace('/memberHome');
+            document.location.replace('/member');
         } else {
             alert(response.statusText);
         }
     }
-
 };
 
 const contactForm = document.querySelector('.contact-form');
