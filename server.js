f1ec227c4ebf7a4bb8153c3f8cd52da90974abb3
themuppetman <157922635+themuppetman@users.noreply.github.com>
2024-05-20 17:55:31 -0700

    PORT correction

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(process.env.PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log('Now listening', PORT));
 });
   
